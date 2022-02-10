7075843ab3f1986d1189543e171a5a3540aa07de
Avaer Kazmer <a@modules.io>
2022-02-10 16:09:29 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+// import easing from './easing.js';
+import metaversefile from 'metaversefile';
+const {useApp, useFrame, useActivate, useLoaders, usePhysics, addTrackedApp, useDefaultModules, useCleanup} = metaversefile;
+
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
+
+export default () => {
+  const app = useApp();
+  const physics = usePhysics();
+
+  app.name = 'turret';
+
+  // let activateCb = null;
+  // let frameCb = null;
+  useActivate(() => {
+    // activateCb && activateCb();
+  });
+  useFrame(() => {
+    // frameCb && frameCb();
+  });
+
+  let physicsIds = [];
+  (async () => {
+    const u = `${baseUrl}turret.glb`;
+    let o = await new Promise((accept, reject) => {
+      const {gltfLoader} = useLoaders();
+      gltfLoader.load(u, accept, function onprogress() {}, reject);
+    });
+    // const {animations} = o;
+    o = o.scene;
+    app.add(o);
+
+    // app.updateMatrixWorld();
+
+    /* let baseMesh = null;
+    o.traverse(o => {
+      if (!baseMesh && o.isMesh && /base_container/i.test(o.name)) {
+        baseMesh = o;
+      }
+    }); */
+    const physicsId = physics.addGeometry(o);
+    physicsIds.push(physicsId);
+  })();
+  
+  useCleanup(() => {
+    for (const physicsId of physicsIds) {
+      physics.removeGeometry(physicsId);
+    }
+  });
+
+  return app;
+};
